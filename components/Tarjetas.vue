bdddcf89e6d2b5036eca18c71f18bc9205506efe
Karly-Dan <daniela.kar99@gmail.com>
2022-09-02 13:27:55 -0500

    se agrega componente de tarjetas

---
+++ components/Tarjetas.vue
@@ -0,0 +1,38 @@
+<template>
+    <section>
+          <div class="titulo-de-tarjeta">
+            <img src="../assets/img/bombilla.png" alt="">
+            <h1>ESTRATEGIA</h1>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi voluptatibus, odit nulla eaque placeat facilis, deleniti non amet tempore, autem totam perspiciatis aperiam quam sunt! Accusamus libero tempore delectus debitis.</p>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  
+  <style scoped>
+      section {
+      width: 265px;
+      height: 355px;
+      box-shadow: 5px 5px 0px 0 rgba(0,0,0,0.2);
+      padding: 30px 20px;
+      text-align: center;
+      font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+      background-color: white;
+      margin-left: 20px;
+  }
+  img {
+    height: 42px;
+    width: 42px;
+    border-radius: 50%;
+    background-color: rgb(224, 229, 235) !important;
+    padding: 18px 18px 18px 18px;
+
+
+  }
+  </style>

f9e457714328abb3c9d68cef37af48aea3bcf8e6
Karly-Dan <daniela.kar99@gmail.com>
2022-09-02 13:27:24 -0500

    se agrega componente de boton

---
+++ components/Boton.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <input type="button" value="Ingresar" class="button1">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style scoped>
+      .button1 {
+    background-color: white; 
+    color: black; 
+    border: 2px solid #df612a;
+    height: 45px;
+    width: 320px;
+    border-radius: 5px;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+.button1:hover {
+    background-color: #df612a;
+    color: white;
+}
+  </style>

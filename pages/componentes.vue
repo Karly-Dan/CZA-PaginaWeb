48e8d6b85f0520486f6cd0329337dcb72115a197
Karly-Dan <daniela.kar99@gmail.com>
2022-09-02 13:26:08 -0500

    se agrega a páginas nueva carpeta componentes.vue para la visualizacion de componentes

---
+++ pages/componentes.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <section class="contenedor-tarjetas">
+            <br>
+            <tarjetas-vue></tarjetas-vue>
+            <br>
+            <br>
+        </section>
+        <section>
+            <br>
+            <boton-vue></boton-vue>
+            <br>
+        </section>
+
+
+    </div>
+</template>
+
+<script>
+    import tarjetasVue from '/components/Tarjetas.vue';
+    import BotonVue from '/components/Boton.vue';
+
+export default {
+    components: {
+        tarjetasVue,
+        BotonVue,
+    }
+}
+</script>
+
+<style>
+    .contenedor-tarjetas {
+        background-color: #df612a;
+    }
+    body {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+</style>

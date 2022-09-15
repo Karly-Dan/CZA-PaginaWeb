aa02842c94ad5f806eb332c00f2a552ea53ce379
Karly-Dan <daniela.kar99@gmail.com>
2022-09-15 12:49:14 -0500

    componente del header principal de la pagina

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img src="./../assets/img/logo.jpeg" alt="">
+    <p>Home</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style  scoped>
+    div {
+        display: flex;
+        justify-content: space-between;
+        height: 90px;
+        margin-right: 10px;
+    }
+    img {
+        height: 90px;
+    }
+</style>

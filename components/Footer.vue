b3d79a989a05ea011dc3f83ea2999e8fc497b7f6
Karly-Dan <daniela.kar99@gmail.com>
2022-09-15 12:50:03 -0500

    se agrega componente del footer principal de la pagina

---
+++ components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Contactanos</h1>
+        <div style="display: flex; justify-content: space-evenly;">
+        <p>correo</p>
+        <p>telefono</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: #25579b;
+        color: white;
+        font-style: italic;
+    }
+    h1 {
+        margin: 0;
+        text-align: center;
+    }
+</style>

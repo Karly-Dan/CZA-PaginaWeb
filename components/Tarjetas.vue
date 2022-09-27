ce5138fc5fb707cde239423cc37e0ca5eb2325a8
Karly-Dan <daniela.kar99@gmail.com>
2022-09-27 18:38:02 -0500

    se agregan dos tarjetas mas al componente y se modifican estilos

--- components/Tarjetas.vue
+++ components/Tarjetas.vue
@@ -3,9 +3,20 @@
         <div class="titulo-de-tarjeta">
           <img src="../assets/img/bombilla.png" alt="">
           <h1>ESTRATEGIA</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi voluptatibus, odit nulla eaque placeat facilis, deleniti non amet tempore, autem totam perspiciatis aperiam quam sunt! Accusamus libero tempore delectus debitis.</p>
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+        </div>
+        <div class="titulo-de-tarjeta2">
+          <img src="../assets/img/bombilla.png" alt="">
+          <h1>ESTRATEGIA</h1>
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+        </div>
+        <div class="titulo-de-tarjeta3">
+          <img src="../assets/img/bombilla.png" alt="">
+          <h1>ESTRATEGIA</h1>
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
         </div>
     </section>
+
 </template>
 
 <script>
@@ -20,32 +31,25 @@ export default {
         flex-wrap: wrap;
         justify-content: center;
         background-color: orangered;
-        padding: 20px;
-        height: 400px;
     }
-    .titulo-de-tarjeta {
+    .titulo-de-tarjeta, .titulo-de-tarjeta2, .titulo-de-tarjeta3 {
       box-shadow: 5px 5px 0px 0 rgba(0,0,0,0.2);
       width: 100%;
       text-align: center;
       font-family: Montserrat, sans-serif !important;
       color: #555c66 !important;
       font-style: italic;
-      height: auto;
-      width: 300px;
       margin-right: 25px;
       margin-left: 25px;
-      margin-bottom: 85px;
       background-color: white;
-      height: auto;
       font-size: 1.25rem;
-      padding: 20px;
+      padding: 30px;
     }
     img {
     height: 42px;
     width: 42px;
     border-radius: 50%;
     background-color: rgb(224, 229, 235) !important;
-    margin-top: 20px;
     padding: 18px;
     }
     p {
@@ -58,15 +62,53 @@ export default {
       font-size: 1.25rem;
       font-weight: 400;
       text-transform: uppercase;
+      color: #555c66 !important;
     }
-    @media (min-width: 600px){
-        .personal {
-            width: 50%;
-        }
+@media (min-width: 300px) {
+    .contenedor{
+      height: 945px;
     }
-  @media (min-width: 1000px){
-    .personal {
-        width: 32%;
+    .titulo-de-tarjeta, .titulo-de-tarjeta2, .titulo-de-tarjeta3 {
+      width: 300px;
+      height: 256px
     }
-  }
+    .titulo-de-tarjeta {
+      margin-top: -115px;
+    }
+}
+@media (min-width: 700px) {
+    .contenedor {
+      height: 1165px;
+    }
+    .titulo-de-tarjeta, .titulo-de-tarjeta2, .titulo-de-tarjeta3 {
+      width: 478px;
+    }
+}
+@media (min-width: 991px){
+    .contenedor {
+      height: 800px;
+    }
+    .titulo-de-tarjeta {
+      width: 335px;
+      margin-top: -50px;
+      margin-bottom: 134px;
+    }
+    .titulo-de-tarjeta2 {
+      width: 335px;
+      margin-bottom: 208px;
+      margin-top: -115px;
+    }
+    .titulo-de-tarjeta3 {
+      width: 335px;
+    }
+}
+@media (min-width: 1199px){
+    .contenedor{
+      height: 400px;
+    }
+    .titulo-de-tarjeta, .titulo-de-tarjeta2, .titulo-de-tarjeta3  {
+      width: 234px;
+      height: 291px;
+    }
+}
 </style>

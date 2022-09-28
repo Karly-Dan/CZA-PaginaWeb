284da1a5cf26c587f2c4e018001fd9eb22061931
Karly-Dan <daniela.kar99@gmail.com>
2022-09-28 18:33:20 -0500

    se modifican los estilos del componente y se agrega otra tarjeta de servicios

--- components/TarjetasDeServicios.vue
+++ components/TarjetasDeServicios.vue
@@ -4,6 +4,22 @@
             <h1>Servicios Contables</h1>
             <img src="../assets/img/signo-de-dolar.png" alt="" class="img2">
         </div>
+        <div class="item1">
+            <h1>Servicios Contables</h1>
+            <img src="../assets/img/signo-de-dolar.png" alt="" class="img2">
+        </div>
+        <div class="item1">
+            <h1>Servicios Contables</h1>
+            <img src="../assets/img/signo-de-dolar.png" alt="" class="img2">
+        </div>
+        <div class="item1">
+            <h1>Servicios Contables</h1>
+            <img src="../assets/img/signo-de-dolar.png" alt="" class="img2">
+        </div>
+        <div class="item1">
+            <h1>Servicios Contables</h1>
+            <img src="../assets/img/signo-de-dolar.png" alt="" class="img2">
+        </div>
     </div>
 </template>
 
@@ -17,31 +33,29 @@ export default {
     .contenedor {
         display: flex;
         flex-wrap: wrap;
-        height: 300px;
+        justify-content: space-around;
     }
     .item1 {
         background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(../assets/img/DINERO.jpg);
         background-repeat: no-repeat;
         width: 100%;
         background-size: 470px;
+        margin-bottom: 20px;
     }
     @media (min-width: 390px){
         .item1 {
-            width: 100%;
+            width: 90%;
+            background-size: 465px;
         }
     }
-    @media (min-width: 600px){
+    @media (min-width: 557px){
         .item1 {
-            width: 50%;
+            width: 49%;
         }
     }
     @media (min-width: 1000px){
     .item1 {
-        width: 30%;
-        background-size: 400px;
-        }
-        .img2 {
-            margin-top: -55px;
+        width: 26%;
         }
     }
     .img2 {
@@ -51,6 +65,8 @@ export default {
         padding-top: 100px;
         filter: invert(99%) sepia(0%) saturate(1136%) hue-rotate(286deg) brightness(116%) contrast(100%);
         opacity: 0.55;
+        margin-top: -45px;
+        padding-bottom: 9px;
     }
     h1 {
         text-align: center;

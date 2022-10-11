96c4b10a6eaf673984c127352637e0d65d219cda
Karly-Dan <daniela.kar99@gmail.com>
2022-10-10 19:41:12 -0500

    se modifican estilos del componente de las tarjetas

--- components/Tarjetas.vue
+++ components/Tarjetas.vue
@@ -1,19 +1,26 @@
 <template>
   <section class="contenedor">
         <div class="titulo-de-tarjeta">
-          <img src="../assets/img/bombilla.png" alt="">
-          <h1>ESTRATEGIA</h1>
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+          <img src="../assets/iconos/reloj-de-arena.png" alt="">
+          <h1>DECLARACIONES</h1>
+          <p>Entrega de declaraciones provisionales, definitivas e informativas en fiempo y forma a la autoridad, que permitan que el contribuyente este al día en su cumplimiento de obligaciones.</p>
         </div>
         <div class="titulo-de-tarjeta2">
-          <img src="../assets/img/bombilla.png" alt="">
-          <h1>ESTRATEGIA</h1>
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+          <img src="../assets/iconos/advertencia.png" alt="">
+          <h1>EVITAR GASTOS</h1>
+          <p>Revisión permanente de sus operaciones para que no generen gastos por actualizaciones, recargos, multas y gastos de ejecución de los impuestos u otras obligaciones establecidas en la Ley, por ejemplo: EFOS o EDOS.</p>
         </div>
         <div class="titulo-de-tarjeta3">
-          <img src="../assets/img/bombilla.png" alt="">
-          <h1>ESTRATEGIA</h1>
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+          <img src="../assets/iconos/martillo-legal.png" alt="">
+          <h1>CUMPLIMIENTO</h1>
+          <p>Actualización de libros de actas de asambleas ordinarias y/o extraordina-rias. emisión de acciones o partes sociales, así como los avisos de privacidad exigidos por la ley, contratos de
+confidencialidad, etc.</p>
+        </div>
+        <div class="titulo-de-tarjeta4">
+          <img src="../assets/iconos/ley.png" alt="">
+          <h1>DEFENSA</h1>
+          <p>Defensa en las Instancias correspondientes en materia fiscal: por determinación de créditos fiscales, bloqueo de sellos digitales, incorporación a la lista
+de EFOS o EDOS, etc.</p>
         </div>
     </section>
 
@@ -32,15 +39,13 @@ export default {
         justify-content: center;
         background-color: #00467a;
     }
-    .titulo-de-tarjeta, .titulo-de-tarjeta2, .titulo-de-tarjeta3 {
+    .titulo-de-tarjeta, .titulo-de-tarjeta2, .titulo-de-tarjeta3, .titulo-de-tarjeta4 {
       box-shadow: 5px 5px 0px 0 rgba(32, 31, 31, 0.51);
       width: 100%;
       text-align: center;
       font-family: Montserrat, sans-serif !important;
       color: #555c66 !important;
       font-style: italic;
-      margin-right: 25px;
-      margin-left: 25px;
       background-color: white;
       font-size: 1.25rem;
       padding: 30px;
@@ -48,7 +53,7 @@ export default {
     img {
     height: 42px;
     width: 42px;
-    border-radius: 50%;
+    border-radius: 39%;
     background-color: rgb(224, 229, 235) !important;
     padding: 18px;
     }
@@ -69,11 +74,12 @@ export default {
     
 @media (min-width: 300px) {
     .contenedor{
-      height: 945px;
+      height: 1550px;
     }
-    .titulo-de-tarjeta, .titulo-de-tarjeta2, .titulo-de-tarjeta3 {
-      width: 300px;
-      height: 256px
+    .titulo-de-tarjeta, .titulo-de-tarjeta2, .titulo-de-tarjeta3, .titulo-de-tarjeta4{
+      width: 260px;
+      height: 320px;
+      margin-bottom: 20px;
     }
     .titulo-de-tarjeta {
       margin-top: -65px;
@@ -81,9 +87,9 @@ export default {
 }
 @media (min-width: 700px) {
     .contenedor {
-      height: 1165px;
+      height: 1600px;
     }
-    .titulo-de-tarjeta, .titulo-de-tarjeta2, .titulo-de-tarjeta3 {
+    .titulo-de-tarjeta, .titulo-de-tarjeta2, .titulo-de-tarjeta3, .titulo-de-tarjeta4 {
       width: 478px;
     }
 }
@@ -111,12 +117,19 @@ export default {
     .contenedor{
       height: 400px;
     }
-    .titulo-de-tarjeta, .titulo-de-tarjeta2, .titulo-de-tarjeta3  {
-      width: 234px;
-      height: 291px;
+    .titulo-de-tarjeta, .titulo-de-tarjeta2, .titulo-de-tarjeta3, .titulo-de-tarjeta4 {
+      width: 220px;
+      height: 355px;
+      margin-right: 35px;
     }
     .titulo-de-tarjeta3 {
       margin-top: -55px;
+    }
+    .titulo-de-tarjeta4 {
+      margin-top: -115px;
+    }
+    .titulo-de-tarjeta {
+      margin-left: 20px;
     }
 }
 </style>

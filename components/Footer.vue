c44903a6c2fa78fa1548008a4df70281ba659a9e
Karly-Dan <daniela.kar99@gmail.com>
2023-01-18 18:29:35 -0600

    se agregan imagenes y enlaces para redes sociales de la empresa en componenete footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
-        <h1>Contáctanos</h1>
         <div class="contenedor">
             <div style="margin-bottom: 35px;">
-                <img src="../assets/img/correo.png" alt="">
-                <p style="margin-top:0;">contacto@condezarate.mx</p>
+                <p><img src="../assets/iconos/gmail.png" alt="">contacto@condezarate.mx</p>
+                <p><img src="../assets/iconos/llamada-telefonica.png" alt="">55 1254 3594</p>
+
             </div>
             <div> 
-                <img src="../assets/img/telefono.png" alt="">
-                <p style="margin-top:0;">55 6285 7771</p>
+                <p>Síguenos en Redes</p>
+                <a href="https://www.instagram.com/condezarateyasociados/?hl=es"><img src="../assets/iconos/instagram.png" alt=""></a>
+                <a href="https://www.facebook.com/people/Conde-Zarate-Asociados-SC/100057309015417/"><img src="../assets/iconos/facebook.png" alt=""></a>
+                <a href="https://www.youtube.com/@condezarateasociados3097"><img src="../assets/iconos/youtube.png" alt=""></a>
             </div>
         </div>
     </div>
@@ -32,6 +34,10 @@ export default {
         margin-top: 0;
         padding-top: 20px;
     }
+    img {
+        margin-right: 5px;
+        margin-bottom: -5px;
+    }
     @media (min-width: 300px){
         .contenedor {
             display: block;
@@ -43,7 +49,7 @@ export default {
     @media (min-width: 700px) {
         .contenedor {
             display: flex;
-            height: 118px;
+            height: 160px;
         }
     }
 </style>

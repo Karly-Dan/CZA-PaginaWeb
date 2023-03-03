67f323936718efa8d2c26e3a6db4e8be3c2e7ca6
Karly-Dan <daniela.kar99@gmail.com>
2023-03-03 12:37:16 -0600

    modificacion del titulo de la pagina

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'CZA-PaginaWeb',
+    title: 'Conde Zárate & Asociados',
     htmlAttrs: {
       lang: 'en'
     },

688539e8dfd140b9ed28788626bf9234b9311607
vikoss <vikoss.yiss@gmail.com>
2024-05-24 12:50:30 -0600

    build: add config to listen server

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,6 +37,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  //
+  server: {
+    host: '0', // default: localhost
+    port: 4000, // default: 3000
+  },
 }
 
